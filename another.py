0746cd67c49a52799d755d955e96242a6a4ad07a
GroupZee <ohara1gus@gmail.com>
2025-05-16 11:47:55 +0300

    Update another.py

--- another.py
+++ another.py
@@ -1,3 +1,4 @@
+#nested while
 choice="yes"
 while choice!="no":
  num=int(input"(Enter any number"))
@@ -6,3 +7,21 @@ while choice!="no":
   print(num,"x",i,"=",num*i)
   i+=1
  choice=input("Enter your choice")
+
+#break
+for x in range(1,68,3):
+ print(x,end=' ')#end= defines how the print output should look like
+ if x==27:
+  break
+
+#continue 
+for x in range(1,68,3):
+ print(x,' ')
+ if x==27:
+  continue 
+
+#Functions
+def display():
+ for x in range(1,68):
+  print(x,end=' ')
+display() 
