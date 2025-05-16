c1a586accf078ec4cf6f11701979457025772532
GroupZee <ohara1gus@gmail.com>
2025-05-15 17:50:47 -0700

    THis is the third.1

--- CTRING_SOUNT.py
+++ CTRING_SOUNT.py
@@ -8,12 +8,19 @@ print("The number of vowels is",v)
 #ord plus a character give ASCII value e.g C=67 ord("C") 
 #chr plus ASCII give character 93=]
 #range(start,stop,step)
-for value in range(0,81):
+for value in range(0,8):
     print(value,end=" ")
-
+print("\n")
 #print alphabet in lower case and upper case
-for v in range(65,92):
+for v in range(65,91,3):
     print(chr(v),end=' ')
-    
+print("\n")
 for v in range(97,123):
-    print(chr(v),end="\n")
+    print(chr(v),end=" ")
+    
+print("\n")   
+#Nested loop
+for f in range(1,11):
+    for g in range(10,21):
+        print(g,end=' ')#prints row
+    print(f,end="\n")#prints column
