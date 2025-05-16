949307f96bdb298880c0dde6b9944e3e1972cc93
GroupZee <ohara1gus@gmail.com>
2025-05-16 12:27:51 +0300

    Update2 another.py

--- another.py
+++ another.py
@@ -1,7 +1,7 @@
 #nested while
 choice="yes"
 while choice!="no":
- num=int(input"(Enter any number"))
+ num=int(input("Enter any number"))
  i=1
  while i<3:
   print(num,"x",i,"=",num*i)
@@ -24,4 +24,22 @@ for x in range(1,68,3):
 def display():
  for x in range(1,68):
   print(x,end=' ')
+def subtract():
+ n1=int(input("Number 1:"))
+ n2=int(input("Number 2: "))
+ if n1>n2:
+  return n1-n2
+ else:
+  return n2-n1
+def pint():
+ print(subtract())
 display() 
+pint()
+#more functions
+def add(n1,m):
+ print("The sum of",n1,"and",m,"is",n1+m)
+def multi(d,a):
+ return d*a
+h=973
+j=7384
+add(h,j)
