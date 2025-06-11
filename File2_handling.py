398367db305a524618dcc2d021dd36b3e9c009cb
GroupZee <ohara1gus@gmail.com>
2025-06-11 13:05:27 -0700

    File reading

--- File2_handling.py
+++ File2_handling.py
@@ -1,4 +1,5 @@
-try:
+def sth():
+ try:
     f=open("Student1.txt","w")
     while True:
         rollno=input("Enter Roll Number:")#should check if it exists
@@ -6,9 +7,59 @@ try:
         course=input("Course: ")#should check if course exists
         print(rollno,name,course,file=f)
         ch=input("Add another?")
-        if ch=="No" or ch=="no" or ch=="NO" or ch=="nO":
+        if ch=='yes' or ch=='Yes' or ch=='YES':
+            continue
+        elif ch=="No" or ch=="no" or ch=="NO" or ch=="nO":#accepts only yes
             break
-except OSError:
+        else:
+            print("I don't understand your answer")
+            break
+ except OSError:
     print("unable to creat file")
-finally:
+ finally:
     f.close()
+def addd():
+    try:
+        f=open("Student1.txt","a")
+        while True:
+            rollno=input("Enter Roll Number:")#should check if it exists
+            name=input("Name: ")
+            course=input("Course: ")#should check if course exists
+            print(rollno,name,course,file=f)
+            ch=input("Add another?")
+            if ch=='yes' or ch=='Yes' or ch=='YES':
+                print("Okay!")
+                continue
+            elif ch=="No" or ch=="no" or ch=="NO" or ch=="nO":#accepts only yes
+                break
+            else:
+                print("I don't understand your answer")
+                break
+    except OSError:
+        print("unable to creat file")
+    finally:
+        f.close()
+#addd()
+def display ():
+    f=None
+    try:
+        f=open("Student1.txt","r")
+        s=f.read()
+        print(s)
+    except OSError:
+        print("File not found")
+    finally:
+        if f:
+            f.close()
+            
+display()
+file_p='Student1.txt'
+from collections import Counter
+def many(file_p):
+    with open(file_p) as file:
+        words=file.read().lower().split()
+        for word, count in Counter(words).most_common(5):
+            print(word,count)
+
+#many(file_p)           
+        
